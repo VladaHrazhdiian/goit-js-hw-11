c0bdba24e0d29e79322eeb50e2135f5381dc3887
VladaHrazhdiian <123592785+VladaHrazhdiian@users.noreply.github.com>
2023-07-10 23:01:05 +0300

    Update pixabayAPI.js

--- src/js/pixabayAPI.js
+++ src/js/pixabayAPI.js
@@ -4,26 +4,31 @@ export default class PixabayAPI {
   #API_KEY = '38110026-5bfbf894cc748013b74eb0441';
   #BASE_URL = 'https://pixabay.com/api/';
 
-  page = 1;
   per_page = 40;
 
   total_hits = null;
   query = null;
 
-  async fetchPhotos(page = this.page) {
-  this.page = page;
+  async fetchPhotos(page = 1) {
+    try {
+      const config = {
+        url: this.#BASE_URL,
+        params: {
+          key: this.#API_KEY,
+          q: this.query,
+          image_type: 'photo',
+          orientation: 'horizontal',
+          safesearch: true,
+          per_page: this.per_page,
+          page: page,
+        },
+      };
 
-  try {
-    return await axios.get(
-      `${this.#BASE_URL}?key=${this.#API_KEY}&q=${
-        this.query
-      }&image_type=photo&orientation=horizontal&safesearch=true&per_page=${
-        this.per_page
-      }&page=${this.page}`
-    );
-  } catch (err) {
-    console.log(err);
-    throw new Error('Oops! Error occurred. Please try again later.');
+      const response = await axios.get(config.url, { params: config.params });
+      return response.data;
+    } catch (err) {
+      console.log(err);
+      throw new Error('Oops! Error occurred. Please try again later.');
+    }
   }
-}
 }
