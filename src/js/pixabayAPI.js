d44ef5b7616d60bb9a0efe5a817de5b05a3e232f
VladaHrazhdiian <123592785+VladaHrazhdiian@users.noreply.github.com>
2023-07-11 22:25:26 +0300

    Update pixabayAPI.js

--- src/js/pixabayAPI.js
+++ src/js/pixabayAPI.js
@@ -4,34 +4,30 @@ export default class PixibayAPI {
   #API_KEY = '38110026-5bfbf894cc748013b74eb0441';
   #BASE_URL = 'https://pixabay.com/api/';
 
-  page = 1; 
+  page = 0;
   per_page = 40;
 
   total_hits = null;
   query = null;
-  prevQuery = null;
+  prevQuery = null; 
 
   async fetchPhotos(page = null) {
     if (page !== null) {
+      
       this.page = page;
     } else {
+  
       this.page += 1;
     }
 
-    const params = {
-      key: this.#API_KEY,
-      q: this.query,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: true,
-      per_page: this.per_page,
-      page: this.page,
-    };
-
     try {
-      const response = await axios.get(this.#BASE_URL, { params });
-
-      return response.data;
+      return await axios.get(
+        `${this.#BASE_URL}?key=${this.#API_KEY}&q=${
+          this.query
+        }&image_type=photo&orientation=horizontal&safesearch=true&per_page=${
+          this.per_page
+        }&page=${this.page}`
+      );
     } catch (err) {
       console.log(err);
     }
